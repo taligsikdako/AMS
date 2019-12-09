aa82d3cb3f3ba9d7c7dba6de92dbb91fdcd248fa
taligsikdako <arjayburlas@gmail.com>
2019-12-09 08:06:09 +0800

    attenandace modific

--- application/views/pages/user/attendance.php
+++ application/views/pages/user/attendance.php
@@ -8,12 +8,12 @@
             <div class="card ">
               <div class="card-header ">
                 <h5 class="card-title">Attendance Monitoring System</h5>
-                <p class="card-category">Select your shift then click login</p>
+                <p class="card-category">attendance logs</p>
               </div>
               <div class="card-body ">
                 <!-- <canvas id=chartHours width="400" height="100"></canvas> -->
                
-                <input type="button" class="btn btn-primary" value="Login">
+               
               </div>
               <div class="card-footer ">
                 <hr>
@@ -25,13 +25,40 @@
         <div class="row">
           <div class="col-md-4">
             <div class="card ">
+              <div class="card-header ">
+                <h5 class="card-title">AMS</h5>
+                <p class="card-category">Select your shift then click login</p>
+              </div>
+              <div class="card-body ">
+              <input type="button" class="btn btn-primary" value="Login">
+              </div>
+              <div class="card-footer ">
             
-           
-          
+                <hr>
+               
+              </div>
             </div>
           </div>
           <div class="col-md-8">
-         
+            <div class="card card-chart">
+              <div class="card-header">
+                <h5 class="card-title">NASDAQ: AAPL</h5>
+                <p class="card-category">Line Chart with Points</p>
+              </div>
+              <div class="card-body">
+                <!-- <canvas id="speedChart" width="400" height="100"></canvas> -->
+              </div>
+              <div class="card-footer">
+                <div class="chart-legend">
+                  <i class="fa fa-circle text-info"></i> Tesla Model S
+                  <i class="fa fa-circle text-warning"></i> BMW 5 Series
+                </div>
+                <hr/>
+                <div class="card-stats">
+                  <i class="fa fa-check"></i> Data information certified
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
